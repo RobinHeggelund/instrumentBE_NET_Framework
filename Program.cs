b2c380f78f9de6a0ff5db7d6bc829aff34b93191
RobinHeggelund <robinheggelund1@gmail.com>
2023-03-09 20:30:11 +0100

    09.03

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,16 +15,29 @@ namespace instrumentBE_NET_Framework
 {
     internal class Program
     {
-        static void Main(string[] args)
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        static void Main(string[] args)
         {
+
+            // Arguments
 
             bool logging = false;
             int com = 3;
             bool background = false;
             int port = 5000;
+            bool skipSetup = false;
+
+            // Setup Variables
+
+            bool settingsUpdatedInArgs = false;
 
             // Check for command line arguments
+
             for (int i = 0; i < args.Length; i++)
             {
                 if (args[i].Equals("-logging"))
@@ -31,6 +45,7 @@ namespace instrumentBE_NET_Framework
                     if (i + 1 < args.Length && bool.TryParse(args[i + 1], out bool result))
                     {
                         logging = result;
+                        settingsUpdatedInArgs = true;
                     }
                 }
                 else if (args[i].Equals("-com"))
@@ -38,6 +53,7 @@ namespace instrumentBE_NET_Framework
                     if (i + 1 < args.Length && int.TryParse(args[i + 1], out int result))
                     {
                         com = result;
+                        settingsUpdatedInArgs = true;
                     }
                 }
                 else if (args[i].Equals("-background"))
@@ -45,6 +61,8 @@ namespace instrumentBE_NET_Framework
                     if (i + 1 < args.Length && bool.TryParse(args[i + 1], out bool result))
                     {
                         background = result;
+                        skipSetup = result;
+                        settingsUpdatedInArgs = true;
                     }
                 }
                 else if (args[i].Equals("-port"))
@@ -52,22 +70,246 @@ namespace instrumentBE_NET_Framework
                     if (i + 1 < args.Length && int.TryParse(args[i + 1], out int result))
                     {
                         port = result;
+                        settingsUpdatedInArgs = true;
                     }
                 }
-            
+                else if (args[i].Equals("-skipSetup"))
+                {
+                    if (i + 1 < args.Length && bool.TryParse(args[i + 1], out bool result))
+                    {
+                        skipSetup = result;
+                        
+                    }
+                }
+            }
 
-            // Output the values
-            Console.WriteLine($"Logging: {logging}");
-            Console.WriteLine($"COM: {com}");
-            Console.WriteLine($"Background: {background}");
-            Console.WriteLine($"Port: {port}");
-        }
+            // Feedback on commandline arguments used
 
-        //TCP Server start
+            if (settingsUpdatedInArgs == true)
+            {
+                Console.WriteLine("\nSettings updated from command line arguments");
+            }
 
-        // make an endpoint for communication:
+            else if (skipSetup == true)
+            {
+                Console.WriteLine("\nSetup procedure skipped");
+            }
+            else
+            {
+                Console.WriteLine("No command line arguments used on startup");
+            }
 
-        IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 5000);
+            // User Setup
+
+            bool userSetup = false;
+            bool userIsAskedWithChangingSettings = true;
+
+            if (skipSetup == true)
+            {
+                userIsAskedWithChangingSettings = false;
+            }
+
+
+
+            // Give user feedback on settings
+
+            Console.WriteLine("\r\n"+"------ Settings -------");
+            Console.WriteLine("Logging: " + logging);
+            Console.WriteLine("Running in background?: " + background);
+            Console.WriteLine("COM port: " + com);
+            Console.WriteLine("Port: " + port);
+            Console.WriteLine("-----------------------");
+
+            // Ask if user want to change settings
+
+            while (userIsAskedWithChangingSettings)
+            {
+                Console.WriteLine("Do you want to change these settings for this session? Y/N");
+                string userChangeSettings = Console.ReadLine();
+
+                if (userChangeSettings == "Y" || userChangeSettings == "y")
+                {
+                    userSetup = true;
+                    userIsAskedWithChangingSettings = false;
+                }
+                
+                else if (userChangeSettings == "N" || userChangeSettings == "n")
+                {
+                    userSetup = false;
+                    userIsAskedWithChangingSettings = false;
+                }
+
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter Y or N.");
+                }
+            }
+
+            // User setup
+
+            while (userSetup)
+            {
+                // Select what settings to change
+
+                Console.WriteLine("Which settings do you want to change? 1: Logging, 2: Background, 3: COM port, 4: Port");
+                string userSettingsToChange = Console.ReadLine();
+               
+                // Change logging settings
+
+                if (userSettingsToChange == "1")
+                {
+                    Console.WriteLine("Do you want to enable logging? Y/N");
+                    string userLogging = Console.ReadLine();
+                    
+                    if (userLogging == "Y" || userLogging == "y")
+                    {
+                        logging = true;
+                        Console.WriteLine("Logging enabled");
+                    }
+                    else if (userLogging == "N" || userLogging == "n")
+                    {
+                        logging = false;
+                        Console.WriteLine("Logging disabled");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Settings not changed.");
+                    }
+                }
+
+                // Change background settings
+
+                else if (userSettingsToChange == "2")
+                {
+                    
+                    Console.WriteLine("Do you want to run in background? Y/N");
+                    string userBackground = Console.ReadLine();
+                    
+                    if (userBackground == "Y" || userBackground == "y")
+                    {
+                        background = true;
+                        Console.WriteLine("Running in background");
+                    }
+                    
+                    else if (userBackground == "N" || userBackground == "n")
+                    {
+                        background = false;
+                        Console.WriteLine("Running in foreground");
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Settings not changed.");
+                    }
+                }
+
+                // Change COM port settings
+
+
+                else if (userSettingsToChange == "3")
+                {
+
+                    bool userChangeComSettings = true;
+
+                    while (userChangeComSettings)
+                    {
+                        Console.WriteLine("Which COM port do you want to use?");
+                        string userCom = Console.ReadLine();
+
+                        try
+                        {
+                            int userComInt = int.Parse(userCom);
+                            com = userComInt;
+                            Console.WriteLine("COM port changed to " + com + "!");
+                            userChangeComSettings = false;
+                        }
+                        catch (FormatException e)
+                        {
+                           
+                            Console.WriteLine("Invalid input. Please enter an integer value for the COM port.");
+                        }
+                    }
+                }
+
+                // Change port settings
+
+                else if (userSettingsToChange == "4")
+                {
+
+                    bool userChangePortSettings = true;
+
+                    while (userChangePortSettings)
+                    {
+                        Console.WriteLine("Which Port do you want to use?");
+                        string userPort = Console.ReadLine();
+
+                        try
+                        {
+                            int userPortInt = int.Parse(userPort);
+                            port = userPortInt;
+                            Console.WriteLine("Port changed to " + port + "!");
+                            userChangePortSettings = false;
+                        }
+                        catch (FormatException e)
+                        {
+
+                            Console.WriteLine("Invalid input. Please enter an integer value for the Port.");
+                        }
+                    }
+                }
+
+                // Invalid input
+
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+                // Ask user if they want to see the updated settings
+
+                Console.WriteLine("Do you want to see the updated settings? Y/N");
+                string userSeeUpdatedSettings = Console.ReadLine();
+
+                if (userSeeUpdatedSettings == "Y" || userSeeUpdatedSettings == "y")
+                {
+                 
+
+                    Console.WriteLine("\r\n" + "------ Settings -------");
+                    Console.WriteLine("Logging: " + logging);
+                    Console.WriteLine("Running in background?: " + background);
+                    Console.WriteLine("COM port: " + com);
+                    Console.WriteLine("Port: " + port);
+                    Console.WriteLine("-----------------------");
+                }
+
+                // Ask if user wants to change more settings
+
+                Console.WriteLine("Do you want to change more settings? Y/N");
+                string userChangeMoreSettings = Console.ReadLine();
+
+                if (userChangeMoreSettings == "Y" || userChangeMoreSettings == "y")
+                {
+                    userSetup = true;
+                }
+                else
+                {
+                    userSetup = false;
+                }
+            }
+
+            // If background is true, hide the console window
+            if (background)
+            {
+                var handle = GetConsoleWindow();
+                ShowWindow(handle, 0); // Hide
+            }
+
+            //TCP Server start
+
+            // make an endpoint for communication:
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 5000);
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             //bind to endpoint and start server
@@ -116,11 +358,19 @@ namespace instrumentBE_NET_Framework
                 // return received data to server
 
 
-                
-
                 if (commandReceivedFE != "")
                 {
-                    client.Send(Encoding.ASCII.GetBytes(serialResponse));
+                    try 
+                    {
+                        client.Send(Encoding.ASCII.GetBytes(serialResponse));
+                    }
+
+
+
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
                 }
 
                 else
